c345eb092f562c585c1d793dba9058e99a5fe8a6
Laboratório FIAP <labsfiap@fiap.com.br>
2026-08-24 22:11:34 -0300

    feat: cria estrutura inicial do app

---
+++ Program.cs
@@ -0,0 +1,11 @@
+Console.Write("Nome do Fa: ");
+string nomeFa = Console.ReadLine() ?? string.Empty;
+
+Console.Write("Quantidade de Dias de Show: ");
+int quantidadeDias = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Cupom de Desconto: ");
+string cupomDesconto = Console.ReadLine() ?? string.Empty;
+
+Console.Write("Pontuacao do Clube de Fidelidade: ");
+int pontuacaoFidelidade = Convert.ToInt32(Console.ReadLine());
